6779c61d4499b201823e0be3b3d1466d8e01f143
Bastian <badmaxyt@gmail.com>
2024-04-21 16:34:47 +0200

    feat(thumbnail): :sparkles: add props to pass fields

--- app/components/thumbnail.tsx
+++ app/components/thumbnail.tsx
@@ -1,25 +1,37 @@
 import Image from "next/image";
 
-export default function Thumbnail() {
+interface ThumbnailProps {
+  name: string;
+  price: string;
+  icon: string;
+  qrcode: string;
+}
+
+export default function Thumbnail({
+  name,
+  price,
+  icon,
+  qrcode,
+}: Readonly<ThumbnailProps>) {
   return (
     <div className="relative bg-white rounded-3xl thumbnail">
       <div className="absolute top-[18px] left-[136px] text-black font-semibold text-lg">
-        Beeper
+        {name}
       </div>
       <div className="absolute top-[72px] left-[136px] font-normal text-black text-lg">
-        💰Gratuit
+        {price}
       </div>
       <Image
         className="absolute object-cover rounded-lg thumbnail-icon"
         alt="App Icon"
-        src="/app-icon.png"
+        src={icon}
         width={98}
         height={98}
       />
       <Image
         className="absolute object-cover thumbnail-qrcode"
         alt="QR Code"
-        src="/qr-code.png"
+        src={qrcode}
         width={72}
         height={72}
       />
